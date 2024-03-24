aded4469e0935d305d7a78ce6261b6c674f40a1a
SKarmakar98 <subhankar_@outlook.com>
2024-03-24 22:11:47 +0530

    .

--- src/Components/NewsBody.js
+++ src/Components/NewsBody.js
@@ -5,19 +5,38 @@ import NewsCrd from "./NewsCrd";
 export default class NewsBody extends Component {
   constructor() {
     super();
-    this.state = { articles: [] };
+    this.state = { articles: [], page: 1,category:"sports" };
   }
   async componentDidMount() {
-    let url =
-"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ed9779fdef0744698a7eaa3c3c4aa39a"
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.state.category}&apiKey=ed9779fdef0744698a7eaa3c3c4aa39a&page=${this.state.page}`;
     let jsonData = await fetch(url);
     let jsonArray = await jsonData.json();
-    console.log(jsonArray);
     this.setState({ articles: jsonArray.articles });
+    // console.log(this.state.page);
   }
+
+  OnPrevioud = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.state.category}&apiKey=ed9779fdef0744698a7eaa3c3c4aa39a&page=${this.state.page - 1}`;
+    let jsonData = await fetch(url);
+    let jsonArray = await jsonData.json();
+    this.setState({ articles: jsonArray.articles, page: this.state.page - 1 });
+    //  this.setState({})
+    console.log(this.state.page + 1);
+  };
+  OnNext = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.state.category}&apiKey=ed9779fdef0744698a7eaa3c3c4aa39a&page=${
+      this.state.page + 1
+    }`;
+    let jsonData = await fetch(url);
+    let jsonArray = await jsonData.json();
+    this.setState({ articles: jsonArray.articles, page: this.state.page + 1 });
+    //  this.setState({})
+    console.log(this.state.page + 1);
+  };
   render() {
     return (
-      <div className="card-group">
+      <>
+        <div className="card-group">
           {this.state.articles.map((element) => (
             <NewsCrd
               url={element.url}
@@ -26,6 +45,24 @@ export default class NewsBody extends Component {
             />
           ))}
         </div>
+        <div className="d-grid gap-2 d-md-flex justify-content-md">
+          <button
+            disabled={this.state.page <= 1}
+            className="btn btn-primary position-absolute mx-4 start-0"
+            type="button"
+            onClick={this.OnPrevioud}
+          >
+            Previous
+          </button>
+          <button
+            className="btn btn-primary position-absolute mx-4 end-0"
+            type="button"
+            onClick={this.OnNext}
+          >
+            Next
+          </button>
+        </div>
+      </>
     );
   }
 }
